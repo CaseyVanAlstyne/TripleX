c39a9643ba8f59cf4236581c1ce7c769eeeee110
Casey VanAlstyne <61564549+CaseyVanAlstyne@users.noreply.github.com>
2020-09-02 14:24:29 -0400

    added preprocessor directive to head of code file. Also included the std cout to print Hello World! to the console.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+// the above #include represents a preprocessor directive in the code, which are instructions to the compiler to, in this case, inlcude the <iostream> library into the code BEFORE compiling, which is why it can be found at the very start of our code file.
+// <iostream> is the header file
+// preprocessor directives do not need to end with a semicolon
+
 int main()
 {
+    std::cout << "Hello, World!";
     return 0;
 }
