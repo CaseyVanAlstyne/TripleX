634df5a0c6ef656c8c15c3e260b77b11bd6b7727
Casey VanAlstyne <61564549+CaseyVanAlstyne@users.noreply.github.com>
2020-09-02 14:39:40 -0400

    added some lore to the game by printing strings in the console.

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,6 +5,14 @@
 
 int main()
 {
-    std::cout << "Hello, World!";
+    std::cout << "You are a secret agent for the United States of America breaking into a secure Russian server room to secure highly classified intelligence regarding a likely nuclear attack...";
+    std::cout << std::endl;
+    std::cout << "In order to continue, you must enter the correct codes...";
+    // the above line is known as an expression statement
+    // std is short for "Standard"
+    // std is in the position of the Namespace (which is used to avoid naming conflicts and collisions)
+    // the :: is the scope operator
+    // cout allows us to write to the terminal and the << tells the compiler to write everything to the right (or what's in the string)
+
     return 0;
 }
