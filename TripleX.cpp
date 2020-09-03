5b2a827feb7c8777b816068e85fc5fc78a3108ec
Casey VanAlstyne <61564549+CaseyVanAlstyne@users.noreply.github.com>
2020-09-02 20:33:41 -0400

    added integers into code file. This includes the use of the addition and multiplicative operators. Also used const to protect the integers previously declared.

--- TripleX.cpp
+++ TripleX.cpp
@@ -8,11 +8,26 @@ int main()
     std::cout << "You are a secret agent for the United States of America breaking into a secure Russian server room to secure highly classified intelligence regarding a likely nuclear attack...";
     std::cout << std::endl;
     std::cout << "In order to continue, you must enter the correct codes...";
+    std::cout << std::endl;
+
     // the above line is known as an expression statement
     // std is short for "Standard"
     // std is in the position of the Namespace (which is used to avoid naming conflicts and collisions)
     // the :: is the scope operator
     // cout allows us to write to the terminal and the << tells the compiler to write everything to the right (or what's in the string)
+
+
+    const int a = 1;
+    const int b = 3;
+    const int c = 6;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    // You have to declare a variable to assign it a value. You cannot assign a value to a variable before the variable has been declared.
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
 
     return 0;
 }
