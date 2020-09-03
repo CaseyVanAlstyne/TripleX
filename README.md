73bb1f8fdc3ad4661a54f40feb8433350db9d988
Casey VanAlstyne <61564549+CaseyVanAlstyne@users.noreply.github.com>
2020-09-03 14:30:31 -0400

    added while loop and parameters to pass the leveldifficulty to printintroduction and playgame

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TripleX
+
+TripleX is a command line number game that utilizes user input. In the fictional setting of the Cold War, the user attempts to infiltrate a secure Russian server for highly classified intelligence. Based on the clues provided, can you stop the escalation between two superpowers?
+
+To start the game, do the following.
+
+In your terminal, run the following code:
+    g++ triplex.cpp -o triplex
+    ./triplex
+
+This will start the game. 
+
+Best of luck!
--- TripleX.cpp
+++ TripleX.cpp
@@ -3,11 +3,11 @@
 // <iostream> is the header file
 // preprocessor directives do not need to end with a semicolon
 
-void PrintIntroduction()
-{
+void PrintIntroduction(int Difficulty) {
     std::cout << std::endl;
     std::cout << "You are a secret agent for the Central Intelligence Agency (CIA) breaking into a secure Russian server room in order to obtain highly classified intelligence regarding a likely nuclear attack...\n";
-    std::cout << "In order to continue without being apprehended by Russian Agents, you must enter the correct code.\n";
+    std::cout << "You are attempting to infiltrate a secure server room on level " << Difficulty;
+    std::cout << "\nIn order to continue without being apprehended by Russian Agents, you must enter the correct code.\n";
 
     // the above lines are known as expression statements
     // an expression (myVariable = 5; for instance) followed by a semicolon is an expression statement
@@ -20,13 +20,12 @@ void PrintIntroduction()
     // cin stands for character input; and the >> is known as the extraction operator
 }
 
-void PlayGame()
-{
-    PrintIntroduction();
+bool PlayGame(int Difficulty) {
+    PrintIntroduction(Difficulty);
 
-    const int CodeA = 1;
-    const int CodeB = 3;
-    const int CodeC = 5;
+    const int CodeA = rand();
+    const int CodeB = rand();
+    const int CodeC = rand();
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
     // lines that declare variables are declaration statements, which is simply code that declares something (variables in the above cases)
@@ -51,21 +50,36 @@ void PlayGame()
     std::cout << std::endl;
 
     // Check to see if player's guess is correct
-    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
-        {
-            std::cout << "You have succesfully infiltrated the server.\n";
-            std::cout << "Good work, Comrade!\n";
-        } else
-        {
-            std::cout << "A deafening siren sounds seconds before heavily-armed agents storm the room in which you are standing.\n";
-            std::cout << "'Ruki vverkh,' they scream, as they aim their AK-47s at your person.\n";
-            std::cout << "Game Over!\n";
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+        std::cout << "Good work, Comrade!\n";
+        std::cout << "You have succesfully extracted a file. Keep going!\n";
+        return true;
+    } else {
+        // std::cout << "You entered the wrong code, Agent. Try again!\n";
+        std::cout << "A deafening siren sounds seconds before heavily-armed agents storm the room in which you are standing.\n";
+        std::cout << "'Ruki vverkh,' they scream, as they aim their AK-47s at your person.\n";
+        std::cout << "Game Over!\n";
+        return false;
         }
     std::cout << std::endl;
 }
 
-int main()
-{
-    PlayGame();
+int main() {
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 4; 
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop game until max difficulty level is completed.
+    {  
+    bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); // Clear any errors
+        std::cin.ignore(); // Discard the buffer
+
+    if (bLevelComplete) {
+        ++LevelDifficulty;
+        }
+        
+    }
+    std::cout << "You got all of the files, Agent. Congratulations on defeating the Russians! Now get out of there! \n";
+
     return 0;
 }
